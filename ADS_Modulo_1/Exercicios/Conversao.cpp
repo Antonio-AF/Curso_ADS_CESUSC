ac0aafa06d9d92699fdc32d62f5b75fccc203b11
Antonio-AF <antonio.farias.002@gmail.com>
2022-08-23 19:39:08 -0300

    Atualização dos Exercicios

---
+++ ADS_Modulo_1/Exercicios/Conversao.cpp
@@ -0,0 +1,20 @@
+/*
+
+1) Fazer um programa em C que pergunte um valor em metros e imprime o correspondente em decímetros e milímetros.
+
+ */
+
+#include <stdio.h>
+#include <strstream>
+#include <locale.h>
+
+
+int main(){
+
+    setlocale(LC_ALL, "Portuguese");
+
+    float M, Mm, Cm;
+
+    printf("Esse programa realiza a conversão de Unidades de Medidas Metros, Centimetros")
+
+}
